44a74da8db94ce5d2323aefc3e93cef72e381e03
Adrian Llerena <a.llerena001@gmail.com>
2018-07-17 16:34:47 -0400

    iteration 1 done

--- recipes.js
+++ recipes.js
@@ -11,12 +11,36 @@ mongoose.connect('mongodb://localhost/recipeApp')
 
 
 const Recipe = new Schema({
-  title: String,
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
   level: {
     type: String,
-    enum: ['Easy Peasy', 'Amateur Chef' - 'UltraPro Chef']
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
   },
-  ingredients: []
+  ingredients: [],
+  cousine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg',
+    duration: {
+      type: Number,
+      min: 0
+    },
+    creator: String,
+    created: {
+      type: Date,
+      default: Date.now()
+    },
 
-
+  }
 });
