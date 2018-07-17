1d6867e0c0e8cac920f7a39a737cfa6bee4ebba8
Adrian Llerena <a.llerena001@gmail.com>
2018-07-16 21:29:58 -0400

    schema halfway

--- recipes.js
+++ recipes.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const Schema = mongoose.Schema;
+const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log('Connected to Mongo!');
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
+
+const Recipe = new Schema({
+  title: String,
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef' - 'UltraPro Chef']
+  },
+  ingredients: []
+
+
+});
