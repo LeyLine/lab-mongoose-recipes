b1c26d693abf3602ded5e6228c6a2d19690e4cf5
Adrian Llerena <a.llerena001@gmail.com>
2018-07-17 20:59:16 -0400

    done

--- recipes.js
+++ recipes.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost/recipeApp')
   });
 
 
-const Recipe = new Schema({
+const recipeSchema = new Schema({
   title: {
     type: String,
     required: true,
@@ -31,16 +31,66 @@ const Recipe = new Schema({
   },
   image: {
     type: String,
-    default: 'https://images.media-allrecipes.com/images/75131.jpg',
-    duration: {
-      type: Number,
-      min: 0
-    },
-    creator: String,
-    created: {
-      type: Date,
-      default: Date.now()
-    },
-
-  }
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  },
 });
+
+const Recipe = mongoose.model('two', recipeSchema);
+
+// Recipe.create({
+//   title: 'turk',
+//   level: 'Easy Peasy',
+//   ingredients: ['Spagetti', 'water', 'tomato sauce', 'oil'],
+//   cousine: 'Italian',
+//   dishType: 'Dish',
+//   duration: 60,
+//   creator: 'Adrian'
+
+// }, function (err, user) {
+//   if (err) console.log('An error happened:', err);
+//   else console.log('The user is saved and its value is: ', user);
+// });
+
+// Recipe.insertMany(data)
+//   .then((recipe) => {
+//     console.log('The recipe is ', recipe);
+//   })
+//   .catch((err) => {
+//     console.log('An error has wrecked you: ', err);
+//   });
+
+
+Recipe.updateOne({
+    title: "Rigatoni alla Genovese"
+  }, {
+    duration: 600
+  })
+  .then((user) => {
+    console.log('Updated that rigga');
+  })
+  .catch((err) => {
+    console.log('An error happened:');
+  });
+
+Recipe.deleteOne({
+    title: "Rigatoni alla Genovese"
+  })
+  .then((user) => {
+    console.log('deleted that carrot');
+  })
+  .catch((err) => {
+    console.log('An error happened:');
+  });
+
+// setTimeout(() => {
+//   mongoose.disconnect();
+// }, 1500);
